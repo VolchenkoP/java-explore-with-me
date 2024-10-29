5d4d32837b6d2358e4ff966a8ec288ffa2980ead
Pavel Volchenko <paulvolchenko@gmail.com>
2024-10-30 00:21:24 +0300

    Поправить по проверкам гита17

--- main-service/src/main/java/ru/practicum/common/config/Configuration.java
+++ main-service/src/main/java/ru/practicum/common/config/Configuration.java
@@ -1,5 +1,6 @@
 package ru.practicum.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +10,11 @@ import ru.practicum.statisticsClient.StatisticsClient;
 @Component
 public class Configuration {
 
-    private final RestTemplateBuilder restTemplateBuilder;
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
 
     @Value("${stats-server.url}")
     private String serverUri;
-
-    public Configuration(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplateBuilder = restTemplateBuilder;
-    }
 
     @Bean
     public StatisticsClient statisticClient() {
