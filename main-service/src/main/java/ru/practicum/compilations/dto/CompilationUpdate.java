c802e6892c7f503bbe2ee0bffb0f8e263b48dc46
Pavel Volchenko <paulvolchenko@gmail.com>
2024-11-04 22:19:40 +0300

    Поправить по замечаниям2

--- main-service/src/main/java/ru/practicum/compilations/dto/CompilationUpdate.java
+++ main-service/src/main/java/ru/practicum/compilations/dto/CompilationUpdate.java
@@ -1,7 +1,5 @@
 package ru.practicum.compilations.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -16,7 +14,7 @@ public class CompilationUpdate {
 
     @Length(max = 50)
     private String title;
-    
+
     private Boolean pinned;
 
     private List<Long> events;
