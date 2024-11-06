bf5522dbc9bdefade6f93a5b8478637b9a9a347b
Pavel Volchenko <paulvolchenko@gmail.com>
2024-11-06 22:12:12 +0300

    fix checkstyle5

--- main-service/src/main/java/ru/practicum/events/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/events/repository/EventRepository.java
@@ -35,19 +35,18 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query(value = "SELECT * " +
             "FROM events AS e " +
-            "WHERE (((e.annotation ILIKE %:text% OR e.description ILIKE %:text%) OR :text IS NULL) " +
-            "AND (e.category IN (:category) OR :category IS NULL) " +
-            "AND (e.paid = CAST(:paid AS boolean) OR :paid IS NULL) " +
-            "AND (e.event_date BETWEEN :rangStart AND :rangeEnd) " +
-            "AND CAST(:isAvailable AS boolean) is TRUE " +
-            "OR (select count(id) from requests AS r WHERE r.event = e.id) < participantsLimit) " +
-            "AND state = 'PUBLISHED') ", nativeQuery = true)
-    List<Event> searchEvents(@Param("text") String text,
-                             @Param("category") List<Integer> category,
-                             @Param("paid") Boolean paid,
-                             @Param("rangStart") LocalDateTime rangStart,
-                             @Param("rangeEnd") LocalDateTime rangeEnd,
-                             @Param("isAvailable") boolean isAvailable,
-                             Pageable pageable);
+            "WHERE (((e.annotation ILIKE %?1% OR e.description ILIKE %?1%) OR ?1 IS NULL) " +
+            "AND (e.category IN (?2) OR ?2 IS NULL) " +
+            "AND (e.paid = CAST(?3 AS boolean) OR ?3 IS NULL) " +
+            "AND (e.event_date BETWEEN ?4 AND ?5 ) " +
+            "AND (CAST(?6 AS BOOLEAN) is TRUE " +
+            "  OR( " +
+            "  select count(id) " +
+            "  from requests AS r " +
+            "  WHERE r.event = e.id) < participants_limit) " +
+            "AND state = 'PUBLISHED') ",
+            nativeQuery = true)
+    List<Event> searchEvents(String text, List<Integer> category, Boolean paid, LocalDateTime rangStart,
+                             LocalDateTime rangeEnd, boolean isAvailable, Pageable pageable);
 }
 
